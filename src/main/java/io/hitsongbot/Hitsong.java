1c243d7d81adba881cebc28e2fb7a15de622e15e
Laurent Nguyen <huutuan.laurent@gmail.com>
2023-03-29 10:41:34 +0200

    Create Hitsong bot

---
+++ src/main/java/io/hitsongbot/Hitsong.java
@@ -0,0 +1,10 @@
+package io.hitsongbot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Hitsong {
+    public static void main(String[] args) throws Exception {
+        JDA jda = JDABuilder.createDefault("").build();
+    }
+}
