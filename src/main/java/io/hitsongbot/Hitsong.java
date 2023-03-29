fab55ff7f7e8072bfc21ba3de57efd0d87522a6b
Laurent Nguyen <huutuan.laurent@gmail.com>
2023-03-29 12:28:07 +0200

    Add slf4j dependency and create PingEvent

--- src/main/java/io/hitsongbot/Hitsong.java
+++ src/main/java/io/hitsongbot/Hitsong.java
@@ -1,10 +1,17 @@
 package io.hitsongbot;
 
+import io.hitsongbot.events.PingEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Hitsong {
-    public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("").build();
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createLight("", GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .setActivity(Activity.watching("YouTube"))
+                .build();
+
+        jda.addEventListener(new PingEvent());
     }
 }
---
+++ src/main/java/io/hitsongbot/events/PingEvent.java
@@ -0,0 +1,17 @@
+package io.hitsongbot.events;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Objects;
+
+public class PingEvent extends ListenerAdapter {
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+        if (message.equalsIgnoreCase("ping")) {
+            if (!Objects.requireNonNull(event.getMember()).getUser().isBot()) {
+                event.getChannel().sendMessage("pong").queue();
+            }
+        }
+    }
+}
